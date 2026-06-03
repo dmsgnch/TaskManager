269d5f6e3b2d66af50c86396eeb4467b9c18f44d
Dmytro Honchar <100141083+dmsgnch@users.noreply.github.com>
2026-06-03 17:19:09 +0200

    add dependencies to initializer

--- TaskManager/AppInitializers/DependencyInjectionInitializer.cs
+++ TaskManager/AppInitializers/DependencyInjectionInitializer.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using System.Windows.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using TaskManager.Core.ApiClients;
@@ -6,8 +7,11 @@ using TaskManager.Core.Providers.Abstracts;
 using TaskManager.Core.Services;
 using TaskManager.Core.Services.Abstracts;
 using TaskManager.Providers;
+using TaskManager.Resources.Core;
 using TaskManager.Services;
 using TaskManager.ViewModels.Abstracts;
+using TaskManager.ViewModels.Dialogs;
+using TaskManager.ViewModels.Windows;
 using TaskManager.Views.Windows;
 
 namespace TaskManager.AppInitializers;
@@ -15,30 +19,39 @@ namespace TaskManager.AppInitializers;
 internal static class DependencyInjectionInitializer
 {
     private const string JsonPlaceholderUri = "https://jsonplaceholder.typicode.com";
-    
+
     internal static ServiceProvider DependencyInjectionInitialize(Dispatcher dispatcher)
     {
         ServiceCollection services = new ServiceCollection();
 
-        #region UI core
-
         services.AddSingleton<IAppConfigProvider, AppConfigProvider>();
-        services.AddSingleton<INavigationService , NavigationService >();
+        services.AddSingleton<INavigationService, NavigationService>();
+        services.AddSingleton(GetApplicationResource<LocalizationManager>("LocalizationManager"));
+        services.AddSingleton<ILocalizationService, LocalizationService>();
 
-        #endregion
-        
         services.AddHttpClient<ITaskApiClient, JsonPlaceholderTaskApiClient>(client =>
         {
             client.BaseAddress = new Uri(JsonPlaceholderUri);
         });
 
         services.AddSingleton<ITaskService, TaskService>();
+        services.AddSingleton<IDialogService, DialogService>();
+        services.AddSingleton<ITaskEditDialogViewModelFactory, TaskEditDialogViewModelFactory>();
 
-        services.AddTransient<MainWindow>();
-        
+        services.AddSingleton<MainWindow>();
+
+        services.AddSingleton<MainWindowViewModel>();
+
         services.AddSingleton<IUiDispatcher>(
             new UiDispatcher(dispatcher));
 
         return services.BuildServiceProvider();
+    }
+
+    private static T GetApplicationResource<T>(string key)
+        where T : class
+    {
+        return Application.Current.Resources[key] as T
+               ?? throw new InvalidOperationException($"Application resource '{key}' was not found.");
     }
 }
